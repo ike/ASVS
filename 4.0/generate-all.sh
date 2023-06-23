1eafe1ecb6b44bd0f2403a114285bf97473a40a0
ike <isaac.lewis@signfracturecare.org>
2023-06-23 15:38:24 -0700

    no index on diff

--- 4.0/generate-all.sh
+++ 4.0/generate-all.sh
@@ -28,7 +28,7 @@ for lang in ${LANGS}; do
     python3 tools/export.py --format csv --language $lang > "$verslong.csv"
     python3 tools/export.py --format csv --language $lang --verify-only true
 
-    if [ -n $(git diff "$verslong.json" "$verslong.flat.json" "$verslong.xml" "$verslong.csv")  ]; then
+    if [ -n $(git diff --no-index "$verslong.json" "$verslong.flat.json" "$verslong.xml" "$verslong.csv")  ]; then
       ./generate_document.sh $lang $vers
     fi
 
